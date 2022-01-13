d964872a79bffb972328ef7213e17a47a2c98573
BelaScarparo <belascarparo19@gmail.com>
2022-01-13 11:10:50 -0300

    Curso JS prof Guanabara

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 72
+console.log(`A velocidadeo do seu carro é ${vel} KM/hr`)
+if (vel > 60){ //condição simples.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança.`)
